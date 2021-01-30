a30166f05067c2196c18791fa0d658baa8f60454
Ihmahmoud <77974878+Ihmahmoud@users.noreply.github.com>
2021-01-31 01:24:23 +0300

    handle with url and set the slug

--- job/models.py
+++ job/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -34,6 +35,15 @@ class Job(models.Model): #table
     category = models.ForeignKey('Category', on_delete=models.CASCADE);#category
     #image = models.ImageField(upload_to='jobs/')
     image = models.ImageField(upload_to=image_upload)
+    
+    slug = models.SlugField(blank=True, null=True) #slug
+    
+    
+    def save(self,*args,**kwargs):
+        
+        self.slug = slugify(self.title) ##logic 
+        
+        super(Job,self).save(*args,**kwargs)
     
     def __str__(self):
         return self.title;
--- job/views.py
+++ job/views.py
@@ -16,9 +16,9 @@ def job_list(request):
     context = {'jobs':page_obj} # template name
     return render(request,'job/job_list.html',context)
 
-def job_detail(request , id):
+def job_detail(request , slug):
     #pass
-    job_detail = Job.objects.get(id=id);
+    job_detail = Job.objects.get(slug=slug);
     context = {'job' : job_detail}
     return render(request,'job/job_detail.html',context)
 
