53e87f1cf894da02039864a222f809af8102a10d
Ihmahmoud <77974878+Ihmahmoud@users.noreply.github.com>
2021-02-01 21:41:00 +0300

    Add Job in Django in the form

--- job/views.py
+++ job/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Job
 from django.core.paginator import Paginator
-from .form import ApplyForm
+from .form import ApplyForm , JobForm
+from django.urls import reverse
 
 # Create your views here.
 def job_list(request):
@@ -33,6 +34,23 @@ def job_detail(request , slug):
     
     context = {'job' : job_detail , 'form1':form}
     return render(request,'job/job_detail.html',context)
+
+
+def add_job(request):
+    
+    if request.method=='POST':
+        form = JobForm(request.POST , request.FILES)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.owner = request.user
+            myform.save()
+            return redirect(reverse('jobs:job_list'))
+    
+    else:
+    
+        form =  JobForm()
+    
+    return render(request,'job/add_job.html',{'form':form})
 
 
     
