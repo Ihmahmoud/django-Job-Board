293db3881ac87faa4be9edfea261dac9af49e51b
Ihmahmoud <77974878+Ihmahmoud@users.noreply.github.com>
2021-02-01 23:53:08 +0300

    add login, logout, password change django

--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    
+    path('accounts/', include('django.contrib.auth.urls')),
+    
     path('admin/', admin.site.urls),
+    
     path('jobs/', include('job.urls',namespace='jobs')),
 ]
 
