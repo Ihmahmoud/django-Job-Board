9796de6c5ed271c9c40a8000907ae91b706e42a4
Ihmahmoud <77974878+Ihmahmoud@users.noreply.github.com>
2021-02-06 22:42:03 +0300

    Serializer Django Rest Framework

--- job/serializers.py
+++ job/serializers.py
@@ -1,1 +1,13 @@
 #### get data from model ------> json
+
+from rest_framework import serializers
+
+from.models import Job
+
+
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = '__all__'
+
--- job/urls.py
+++ job/urls.py
@@ -1,8 +1,8 @@
 
 from django.urls import path , include
 
-from . import views
-
+from .import views
+from .import api
 
 app_name ='job'
 
@@ -11,4 +11,8 @@ urlpatterns = [
     path('',views.job_list, name='job_list'),
     path('add',views.add_job, name='add_job'),
     path('<str:slug>',views.job_detail, name='job_detail'),
+    
+    
+    ## api
+    path('api/list',api.joblistapi, name='joblistapi'),
 ]
