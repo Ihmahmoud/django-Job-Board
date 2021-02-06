8534bf764c14a710a1a15c212e141d0345af677d
Ihmahmoud <77974878+Ihmahmoud@users.noreply.github.com>
2021-02-06 20:38:46 +0300

    Contact Form

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'job',
     'bootstrap4',
     'django_filters',
+    'contact',
 ]
 
 MIDDLEWARE = [
@@ -132,4 +133,13 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'IbrahimHamoudMahmoud@gmail.com'
+EMAIL_HOST_PASSWORD = 'wojitqjrwtpamnaa'
+EMAIL_USE_TLS = True
+EMAIL_PORT = '587'
+
 
